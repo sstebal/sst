e25c1389dccbe75f31b9314063f881c98c421405
sstebal <fwefjknefbj@GMail.com>
2024-04-13 11:35:19 +0300

    программа

---
+++ Unit1.pas
@@ -0,0 +1,52 @@
+﻿Unit Unit1;
+
+interface
+
+uses System, System.Drawing, System.Windows.Forms;
+
+type
+  Form1 = class(Form)
+  {$region FormDesigner}
+  internal
+    {$resource Unit1.Form1.resources}
+    button1: Button;
+    {$include Unit1.Form1.inc}
+  {$endregion FormDesigner}
+  public
+    constructor;
+    begin
+      InitializeComponent;
+    end;
+  end;
+
+implementation
+program FastPower;
+
+function Power(base, exponent: integer): integer;
+var
+  result: integer;
+begin
+  result := 1;
+  while exponent > 0 do
+  begin
+    if exponent mod 2 = 1 then
+      result := result * base;
+    base := base * base;
+    exponent := exponent div 2;
+  end;
+  Power := result;
+end;
+
+var
+  base, exponent: integer;
+
+begin
+  writeln('Enter base: ');
+  readln(base);
+  writeln('Enter exponent: ');
+  readln(exponent);
+  
+  writeln(base, ' raised to the power of ', exponent, ' is ', Power(base, exponent));
+end.
+
+end.
